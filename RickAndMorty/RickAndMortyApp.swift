78a149bb3284c46b31e093adee2fcef75798a2c6
Ali Alaa <ali_alaa@hotmail.co.uk>
2025-02-04 17:59:53 +0200

    Project init

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by ali alaa on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
